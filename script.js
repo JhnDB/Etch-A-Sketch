5530e7ea9c301fe3511d56f52e41fffc25499fd9
jhndb <jem.baal2006@gmail.com>
2025-06-24 17:19:10 +0800

    Add basic function

---
+++ script.js
@@ -0,0 +1,17 @@
+function createGrid(num=16) {
+    const container = document.querySelector("#container");
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            const box = document.createElement("div");
+            box.setAttribute("class", "pixel");
+            box.style.setProperty("width", `calc(var(--size) / ${num})`);
+            box.style.setProperty("height", `calc(var(--size) / ${num})`);
+            container.appendChild(box);
+        }
+    }
+    container.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+    });
+}
+
+createGrid(100);
